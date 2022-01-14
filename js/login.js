8acc2cf25a35782ac283435f77fb7aa358f0ac63
SmallRay <t109000000@ntut.edu.tw>
2022-01-15 01:52:36 +0800

    1.0.1

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ loginForm.addEventListener("submit", async e => {
     e.preventDefault();
     const account = await searchData(loginForm["username"].value);
     if (account.size > 0 && account.docs[0].data().password == loginForm["password"].value)
-        window.location.replace("./html/welcome.html");
+        window.location.replace("./html/kagamine.html");
     else
         alert("Woops! Username or Password is Wrong.");
 })
